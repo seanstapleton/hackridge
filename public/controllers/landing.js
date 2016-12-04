3c86703144863bc10833b20d8cb240fd66dd75bf
Alpri Else <alprielse@gmail.com>
2016-12-04 11:58:20 -0600

    shit ton

--- public/controllers/landing.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  var app = angular.module('hackridge');
-
-  app.controller('LandingController', ['$scope', '$http', function($scope, $http) {
-    $scope.check = "Yo";
-  }]);
-}());
