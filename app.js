29f623b7f590fde210899ee20645ec9c8e213965
Sean Stapleton <seanstapleton100@gmail.com>
2017-02-03 19:56:26 -0600

    stopped http request logs

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ module.exports = function(db) {
 
   // uncomment after placing your favicon in /public
   //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-  app.use(logger('dev'));
+  //app.use(logger('dev'));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(cookieParser());
