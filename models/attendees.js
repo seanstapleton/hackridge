c06c66411585b4620e78dfee43399904eb7342df
Sean Stapleton <seanstapleton100@gmail.com>
2017-07-25 13:14:43 -0500

    cleared dir

--- models/attendees.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var attendeesSchema = new Schema({
-    data: Object
-});
-
-module.exports = mongoose.model('Attendee', attendeesSchema);
--- public/controllers/countdown.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var app = angular.module('hackridge');
-    app.controller('CountdownController', ['$scope','$interval', function($scope, $interval) {
-        $interval(function() {
-            $scope.countdown = countdown(new Date("Mar 5 2017 12:00:00"));
-        }, 1000,0);
-
-    }]);
-}());
