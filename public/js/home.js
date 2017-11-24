bab3f3301d037f0d35f73d4bdbd1b46f05f0a0b8
Sean Stapleton <seanstapleton100@gmail.com>
2017-11-24 13:03:34 -0600

    updated style

--- public/js/home.js
+++ public/js/home.js
@@ -5,13 +5,15 @@
 
     $.getJSON("/data/faq.json", function(res) {
       faqs = res;
+      var faq_container = $("<div class='faq-container-div'></div>");
       for (var i = 0; i < faqs.length; i++) {
         var container = $("<div class='faq-container'></div>");
         var question = $("<h2></h2>").text(faqs[i].Q);
         var answer = $("<p></p>").text(faqs[i].A);
         container.append(question,answer);
-        $("#faq").append(container);
+        faq_container.append(container);
       }
+      $("#faq").append(faq_container);
     });
 
 
