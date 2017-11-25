ef75fb49a9b89bdabf055e4b1e2ff2cadfbd2d4d
Sean Stapleton <seanstapleton100@gmail.com>
2017-11-25 14:20:31 -0600

    stuff

--- public/js/home.js
+++ public/js/home.js
@@ -16,6 +16,34 @@
       $("#faq").append(faq_container);
     });
 
+    AOS.init({
+      duration: 1000
+    });
+
+    $.fn.extend({
+      animateCss: function (animationName) {
+          var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+          this.addClass('animated ' + animationName).one(animationEnd, function() {
+              $(this).removeClass('animated ' + animationName);
+          });
+      }
+    });
+
+    $(function() {
+      $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 2000);
+            return false;
+          }
+        }
+      });
+    });
+
 
   });
 }());
