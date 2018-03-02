257e0e549849488d2e47b02b18af9a33b578bfa2
rprendergast180 <rprendergast180@s207.org>
2018-03-02 16:24:35 -0600

    favicon

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ module.exports = function(db) {
   app.set('view engine', 'jade');
 
   // uncomment after placing your favicon in /public
-  //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-  //app.use(logger('dev'));
+  app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+  // app.use(logger('dev'));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(cookieParser());
