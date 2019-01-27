bbc18fb8de82b6df640f4393566f065ed4130e4d
Sean Stapleton <seanstapleton100@gmail.com>
2019-01-27 08:08:53 -0500

    DB change

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-var url = 'mongodb://'+process.env.db_user+':'+process.env.db_pass+'@ds155718.mlab.com:55718/hackridge';
+var url = 'mongodb://'+process.env.db_user+':'+process.env.db_pass+'@ds213715.mlab.com:13715/hackridge';
 mongoose.connect(url);
 
 module.exports = mongoose.connection;
